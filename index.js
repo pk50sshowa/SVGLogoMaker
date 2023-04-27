0acd7a59507055b744ba9f4b78620f5860b83daa
Paul Koehler <paul.koehler@gmail.com>
2023-04-27 16:10:29 -0700

    modularized cli and inquirer functions

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+const CLI = require('./lib/cli.js');
 
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+const cli = new CLI();
 
-inquirer
-  .prompt([
-    {
-        type: 'maxlength-input',
-        message: 'Please enter three characters.',
-        name: 'characters',
-        maxLength: 3,
-    },
-    {
-        type: 'input',
-        message: 'What text color would you like?',
-        name: 'textColor',
-    },
-    {
-        type: 'list',
-        message: 'What shape would you like to choose?',
-        choices: ['Circle', 'Triangle', 'Square'],
-        name: 'shapes',
-    },
-    {
-        type: 'input',
-        message: 'What text color would you like?',
-        name: 'shapeColor',
-    }
-  ]).then(console.log)
-
-//   .then((response) =>
-//     writeFile(`logo.svg`, JSON.stringify(response),  err => { 
-//         if (err) {
-//             throw err;
-//         }
-//         console.log ('Generated logo.svg');
-//     } )nod
-//   );
+cli.run();
