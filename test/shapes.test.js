b41a12254eed15c04f7ea63e5c03a34b1d7cafa3
Paul Koehler <paul.koehler@gmail.com>
2023-05-01 18:55:03 -0700

    testing suite and package.json modified

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,11 +1,11 @@
-const {Circle, Square, Triangle} = require("./shapes")
+const {Circle, Square, Triangle} = require("../lib/shapes")
 
 describe('Circle', () => {
     test('Renders correctly', () => {
         const shape = new Circle();
         var color =('blue')
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}">`);
+        expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" fill="${color}" />`);
     });
   });
   describe('Square', () => {
@@ -13,7 +13,7 @@ describe('Circle', () => {
         const shape = new Square();
         var color =('green')
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<rect x="50" height="200" width="200" fill="${color}">`);
+        expect(shape.render()).toEqual(`<rect x="90" y="40" width="200" height="200" fill="${color}" />`);
       });
     });
   describe('Triangle', () => {
@@ -21,6 +21,6 @@ describe('Circle', () => {
         const shape = new Triangle();
         var color =('yellow')
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${color}">`);
+        expect(shape.render()).toEqual(`<polygon points="0,200 300,200 150,0" fill="${color}" />`);
       });
     });
